f5d45da9ef5e332f805b40d4ad6d0c62765eb110
Thanachin <143328453+ThanachinPronmmana@users.noreply.github.com>
2025-03-05 20:46:35 +0700

    Fix User

--- repositories/UserRepository.py
+++ repositories/UserRepository.py
@@ -11,25 +11,31 @@ users_collection = db.users
 class UserRepository:
     @staticmethod
     async def get_all_users():
-        return await users_collection.find().to_list(None)
+        users = await users_collection.find().to_list(None)
+        return [User(**user, id=str(user["_id"])) for user in users]  # แปลงเป็น Pydantic model
 
     @staticmethod
-    async def create_user(user_data):
-        result = await users_collection.insert_one(user_data)
-        return {"id": str(result.inserted_id), **user_data}
+    async def create_user(user_data: User):
+        user_dict = user_data.model_dump(exclude={"id"})  # แปลงเป็น dict (ยกเว้น id)
+        result = await users_collection.insert_one(user_dict)
+        return User(id=str(result.inserted_id), **user_dict)  # คืนค่าเป็น User model
 
     @staticmethod
-    async def get_user_by_id(user_id):
-        return await users_collection.find_one({"_id": ObjectId(user_id)})
+    async def get_user_by_id(user_id: str):
+        user = await users_collection.find_one({"_id": ObjectId(user_id)})
+        return User(**user, id=str(user["_id"])) if user else None
 
     @staticmethod
-    async def update_user(user_id, user_data):
-        return await users_collection.find_one_and_update(
+    async def update_user(user_id: str, user_data: User):
+        user_dict = user_data.model_dump(exclude={"id"})
+        updated_user = await users_collection.find_one_and_update(
             {"_id": ObjectId(user_id)},
-            {"$set": user_data},
+            {"$set": user_dict},
             return_document=True
         )
+        return User(**updated_user, id=str(updated_user["_id"])) if updated_user else None
 
     @staticmethod
-    async def delete_user(user_id):
-        return await users_collection.find_one_and_delete({"_id": ObjectId(user_id)})
+    async def delete_user(user_id: str):
+        deleted_user = await users_collection.find_one_and_delete({"_id": ObjectId(user_id)})
+        return User(**deleted_user, id=str(deleted_user["_id"])) if deleted_user else None
